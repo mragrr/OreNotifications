1bde21bd54db3ce3a31b8f0650c88b138ea9cf23
mragrr <meacool@mail.ru>
2026-07-22 21:05:01 +0400

    1.0

---
+++ src/main/java/me/plugins/oreNotifications/OreNotifications.java
@@ -0,0 +1,23 @@
+package me.plugins.oreNotifications;
+
+import me.plugins.oreNotifications.events.DefaultEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OreNotifications extends JavaPlugin {
+    private final JavaPlugin plugin = this;
+
+    @Override
+    public void onEnable() {
+        // 1.2 Реализация команд топа игроков по блокам
+        // 1.3 Реализация возможности добавления Scoreboard с топом
+        // 1.3 Персональные (каждый + в месте) и глобальные сообщения (изменение топ-1) о изменении положения в топе
+        // 1.4 Привелегии для топ игроков
+        Bukkit.getPluginManager().registerEvents(new DefaultEvent(plugin), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/plugins/oreNotifications/events/DefaultEvent.java
@@ -0,0 +1,45 @@
+package me.plugins.oreNotifications.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DefaultEvent implements Listener {
+    private JavaPlugin plugin;
+
+    public DefaultEvent(JavaPlugin p) {
+        plugin = p;
+    }
+
+    public int editConfig(String playerName, String blockName) {
+        int count = plugin.getConfig().getInt("players." + playerName + "." + blockName, 0);
+        count = count + 1;
+        plugin.getConfig().set("players." + playerName + "." + blockName, count);
+        plugin.saveConfig();
+        return count;
+    }
+
+    @EventHandler
+    public void PlayerEditBlock(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        Material material = block.getBlockData().getMaterial();
+        String materialName = material.name();
+        // 1.1 Реализовать добавление отслеживаемых блоков и их lang-индивидуальностей в конфиге
+        if (materialName.equals("DIAMOND_ORE") || materialName.equals("DEEPSLATE_DIAMOND_ORE")) {
+            String playerName = event.getPlayer().getName();
+            int count = editConfig(playerName, "diamond");
+            // 1.1 Реализовать написание только какого то по счёту блока
+            // 1.2 Реализовать написание только последнего добытого за какой-то тайм блока
+            Bukkit.broadcastMessage("Игрок " + playerName + " сломал алмазную руду. Счётчик руд: " + count);
+        }
+        if (materialName.equals("ANCIENT_DEBRIS")) {
+            String playerName = event.getPlayer().getName();
+            int count = editConfig(playerName, "derbis");
+            Bukkit.broadcastMessage("Игрок " + playerName + " сломал древние осколки. Счётчик осколков: " + count);
+        }
+    }
+}
