64948c35e0a26eda5b94ea8d6feb526cd9e1873d
mragrr <meacool@mail.ru>
2026-07-24 03:43:08 +0400

    Added config blocks in Events

--- src/main/java/me/plugins/oreNotifications/events/DefaultEvent.java
+++ src/main/java/me/plugins/oreNotifications/events/DefaultEvent.java
@@ -3,44 +3,86 @@ package me.plugins.oreNotifications.events;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class DefaultEvent implements Listener {
+    // TODO Защита от шёлкового касания
+    // TODO Реализовать написание только какого-то по счёту блока
+    // TODO Реализовать написание только последнего добытого за какой-то тайм блока
     private JavaPlugin plugin;
 
     public DefaultEvent(JavaPlugin p) {
         plugin = p;
     }
 
+    // Запись изменения в счетчике и возврат текущего числа
     public int editConfig(String playerName, String blockName) {
+        // Получаем
         int count = plugin.getConfig().getInt("players." + playerName + "." + blockName, 0);
+
+        // Добавляем текущее изменение
         count = count + 1;
         plugin.getConfig().set("players." + playerName + "." + blockName, count);
+
+        // Сохраняем конфиг после изменения
         plugin.saveConfig();
+
         return count;
     }
 
     @EventHandler
     public void PlayerEditBlock(BlockBreakEvent event) {
-        Block block = event.getBlock();
-        Material material = block.getBlockData().getMaterial();
-        String materialName = material.name();
-        // TODO 1.1 Реализовать добавление отслеживаемых блоков и их lang-индивидуальностей в конфиге
-        // TODO 1.1 Защита от шёлкового касания
-        if (materialName.equals("DIAMOND_ORE") || materialName.equals("DEEPSLATE_DIAMOND_ORE")) {
-            String playerName = event.getPlayer().getName();
-            int count = editConfig(playerName, "diamond");
-            // TODO 1.1 Реализовать написание только какого то по счёту блока
-            // TODO 1.2 Реализовать написание только последнего добытого за какой-то тайм блока
-            Bukkit.broadcastMessage("Игрок " + playerName + " сломал алмазную руду. Счётчик руд: " + count);
+        Block currentBlock = event.getBlock(); // Блок
+        Material material = currentBlock.getBlockData().getMaterial(); // Текущий материал
+        String currentMaterialName = material.name(); // Название материала
+
+        // Секция блоков
+        ConfigurationSection blocksSection = plugin.getConfig().getConfigurationSection("blocks");
+
+        // Проверка на отсутствие элементов
+        if (blocksSection == null) {
+            return;
         }
-        if (materialName.equals("ANCIENT_DEBRIS")) {
-            String playerName = event.getPlayer().getName();
-            int count = editConfig(playerName, "derbis");
-            Bukkit.broadcastMessage("Игрок " + playerName + " сломал древние осколки. Счётчик осколков: " + count);
+
+        // Перебор блоков
+        for (String blockName : blocksSection.getKeys(false)) {
+            // Секция информации блока
+            ConfigurationSection blockInfo = blocksSection.getConfigurationSection(blockName);
+
+            // Проверка на отсутствие элементов внутри блока
+            if (blockInfo == null) {
+                return;
+            }
+
+            // Перебираем допустимые имена материала соответствующие данному блоку
+            List<String> materialNames = blockInfo.getStringList("materialNames");
+            for (String materialName : materialNames) {
+                // При сходстве с сломанным блоком
+                if (materialName.equals(currentMaterialName)) {
+                    String playerName = event.getPlayer().getName(); // Получаем никнейм игрока, который вызвал ивент
+                    int count = editConfig(playerName, blockName); // Регистрируем сломанный блок в конфиг и записываем текущее значение счетчика
+                    String rawNotifyMessage = blockInfo.getString("message"); // Парсим из конфига сообщение о добыче блока
+
+                    // Проверка на отсутствие сообщения
+                    if (rawNotifyMessage == null) {
+                        return;
+                    }
+
+                    // Преобразование сообщения
+                    String notifyMessage = rawNotifyMessage
+                            .replace("{NAME}", playerName)
+                            .replace("{COUNT}", String.valueOf(count));
+
+                    // Вывод сообщения
+                    Bukkit.broadcastMessage(notifyMessage);
+                }
+            }
         }
     }
 }
